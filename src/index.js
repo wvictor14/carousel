7bb5d50d657ce743d47cc0730b111c1c751fe9bb
Victor Yuan <victor.2wy@gmail.com>
2025-08-19 21:55:52 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./styles.css";
+
+import { initializeDropdowns } from "@victor2wy/basic-dropdown"
 export function carousel() {
 }
 
 // carousel();
+// Temporary debugging - add this to your index.js
+initializeDropdowns();
