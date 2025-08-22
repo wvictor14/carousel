dbe7e8fa214733c83dacf1965dbada3e6e7475be
Victor Yuan <victor.2wy@gmail.com>
2025-08-21 21:41:30 -0700

    outline carousel

--- src/index.js
+++ src/index.js
@@ -7,3 +7,9 @@ export function carousel() {
 // carousel();
 // Temporary debugging - add this to your index.js
 initializeDropdowns();
+
+
+// initialize carousel items with the hidden class
+// first element is shown at beginning
+// put images into an array
+// functions that cycle through --> next and previous
