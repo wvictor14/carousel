4f97ca727ea1043b3dcdb5db5ecbd13730d90b4d
Victor Yuan <victor.2wy@gmail.com>
2025-08-21 22:17:07 -0700

    finish carousel

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ function initializeCarousel() {
   const carouselItems = carousel.querySelectorAll(".carousel-item");
   if (!carouselItems.length) return;
 
+  // make first element visible, and the rest are hidden
   for (let i = 0; i < carouselItems.length; i++) {
-
     if (i == 0) {
       carouselItems[i].classList.add('visible');
       carouselItems[i].classList.remove('hidden');
@@ -19,7 +19,59 @@ function initializeCarousel() {
       carouselItems[i].classList.add('hidden');
     }
   }
+
+  // inject the buttons
+  const btnDiv = document.createElement('div');
+  btnDiv.classList.add('btn-container');
+  const btnPrev = document.createElement('button');
+  btnPrev.classList.add('btn');
+  btnPrev.textContent = 'Previous';
+  const btnNext = document.createElement('button');
+  btnNext.classList.add('btn');
+  btnNext.textContent = 'Next';
+
+  const handleClick = function (type = 'next') {
+
+    // current image hidden;
+    carouselItems[state].classList.remove('visible');
+    carouselItems[state].classList.add('hidden');
+
+    // state is the image index
+    // if reached the end of the images, go back to first
+    if (type == 'next') {
+      if (carouselItems.length == state + 1) {
+        state = 0;
+      } else {
+        state += 1;
+      }
+    }
+
+    if (type == 'previous') {
+      if (state - 1 == -1) {
+        state = carouselItems.length - 1;
+      } else {
+        state -= 1;
+      }
+    }
+
+    // new image visible
+    carouselItems[state].classList.remove('hidden');
+    carouselItems[state].classList.add('visible');
+
+  }
+  btnPrev.addEventListener("click", () => { handleClick('previous') });
+  btnNext.addEventListener("click", () => { handleClick("next") });
+
+  btnDiv.appendChild(btnPrev);
+  btnDiv.appendChild(btnNext);
+  carousel.appendChild(btnDiv);
+
+  // btn listeners change state
+
 }
+
+
+let state = 0;
 
 // carousel();
 // Temporary debugging - add this to your index.js
